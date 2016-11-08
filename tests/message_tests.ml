f01429d64d988e35de9f3e0c6538b6561cd1cde0
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-08 21:21:17 +0000

    Modularises message tests

--- tests/message_tests.ml
+++ tests/message_tests.ml
@@ -1,14 +1,17 @@
 open Core.Std
-open Messages.Message
 
-let message_types_of_string () =
-  let variants  = [DH_INIT  ; DH_REPLY  ] in
-  let strings   = ["dh-init"; "dh-reply"] in
-  let strings' = List.map variants ~f:(Messages.Message.to_string) in
-  List.iter2_exn strings strings' ~f:(Alcotest.(check string) "Checking deserialised variant")
+module Message_variant = struct
+  open Messages.Message
+  
+  let test_of_string () =
+    let variants  = [DH_INIT  ; DH_REPLY  ] in
+    let strings   = ["dh-init"; "dh-reply"] in
+    let strings' = List.map variants ~f:(Messages.Message.to_string) in
+    List.iter2_exn strings strings' ~f:(Alcotest.(check string) "Checking deserialised variant")
 
-let unit_tests = [
-  ("Message variants can be deserialised", `Quick, message_types_of_string)
-]
+  let unit_tests = [
+    ("Message variants can be deserialised", `Quick, test_of_string)
+  ]
+end
 
-let () = Alcotest.run "Tests for the Messages module" [("Unit tests",unit_tests)]
+let () = Alcotest.run "Tests for the Message variants" [("Message variant unit tests", Message_variant.unit_tests)]
