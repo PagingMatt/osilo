b7984d8a20f51791fbe30c3087c5c50cf1f62e77
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-15 19:36:14 +0000

    Fixes comments

--- src/Peer.mli
+++ src/Peer.mli
@@ -2,12 +2,10 @@ type t
 (* Internal representation for a peer *)
 
 val host : t -> string
-(** Gets host from a peer 
- * TODO consider returning ipaddr or uri *)
+(** Gets host from a peer. TODO consider returning ipaddr or uri *)
 
 val port : t -> int
-(** Gets port from peer
- * TODO consider if int is appropriate - probably is *)
+(** Gets port from peer. TODO consider if int is appropriate - probably is *)
 
 val compare : t -> t-> int
 (** Compare two peers - needed for Map.OrderedType satisfaction (keys in cache) *)
