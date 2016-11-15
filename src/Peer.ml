7feefbab761d7fe2d6bc56101c4f43338212e83b
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-15 21:17:52 +0000

    Use URI compare

--- src/Peer.ml
+++ src/Peer.ml
@@ -7,12 +7,7 @@ let host p = p.host
 
 let port p = p.port
 
-let combine_error i j =
-  if i=0 || j=0 then i + j
-  else i * j 
-
 let compare p1 p2 =
-  let hosts = String.compare p1.host p2.host in
-  let ports = p1.port - p2.port in
-  if (hosts=0) && (ports=0) then 0
-  else combine_error hosts ports
+  let uri1 = Uri.make ~host:(host p1) ~port:(port p1) () in
+  let uri2 = Uri.make ~host:(host p2) ~port:(port p2) () in
+  Uri.compare uri1 uri2
