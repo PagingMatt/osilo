7156b16602f7234391a37b6b3048f59d467bca61
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-15 19:34:57 +0000

    Improves documentation via ocamldoc

--- src/Cryptography.mli
+++ src/Cryptography.mli
@@ -2,48 +2,39 @@ open Nocrypto
 open Lwt
 open Lwt.Infix
 
-(* Generic exception to throw when key exchange fails anywhere
- * TODO add information to this to propogate up stack *)
+(** Generic exception to throw when key exchange fails anywhere. TODO add information to this to propogate up stack *)
 exception Key_exchange_failed
 
-(* Keying service carries out operations on functional key cache *)
+(** Keying service carries out operations on functional key cache *)
 module KS : sig
   type t
   (* Internal representation fo the keying service *)
 
   val empty : capacity:int -> t
-  (* Constructor for keying service with an empty cache *)
+  (** Constructor for keying service with an empty cache *)
 
   val invalidate    : ks:t -> peer:Peer.t -> t 
-  (* Returns a keying service with a given peer removed from its key cache*)
+  (** Returns a keying service with a given peer removed from its key cache*)
 
   val mediate       : ks:t -> peer:Peer.t -> group:Dh.group -> public:Cstruct.t -> t * Cstruct.t
-  (* Given a keying service, peer, DH group and public key, will carry out DH exchange to return a
-   * keying service, private key pair
-   * TODO just return keying service *)
+  (** Given a keying service, peer, DH group and public key, will carry out DH exchange to return a keying service, private key pair. TODO just return keying service *)
 
   val lookup        : ks:t -> peer:Peer.t -> Cstruct.t option
-  (* Given a keying service and peer give the shared private key for that peer if it exists *)
+  (** Given a keying service and peer give the shared private key for that peer if it exists *)
 
   val lookup'       : ks:t -> peer:Peer.t -> (t * Cstruct.t) Lwt.t
-  (* Given a keying service and peer, transparently return a promise for a keying service, private 
+  (** Given a keying service and peer, transparently return a promise for a keying service, private 
     * key pair *)
 end
 
-(* Cryptography service depends on a keying service and encrypts and decrypts Cstruct.t messages 
- * per Peer.t *)
+(** Cryptography service depends on a keying service and encrypts and decrypts Cstruct.t messages per Peer.t *)
 module CS : sig
   val encrypt : ks:KS.t -> peer:Peer.t -> plaintext:Cstruct.t -> (KS.t * Cstruct.t * Cstruct.t) Lwt.t
-  (* Given a keying service, peer and Cstruct plaintext return a promise for a tuple containing a 
-   * keying service (with this peer contained), the Cstruct ciphertext and the Cstruct initial 
-   * vector
-   * TODO type this in a record? *)
+  (** Given a keying service, peer and Cstruct plaintext return a promise for a tuple containing a keying service (with this peer contained), the Cstruct ciphertext and the Cstruct initial vector. TODO type this in a record? *)
 
   exception Decryption_failed
-  (* In the case that decryption fails, this exception is thrown 
-   * TODO encode more information*)
+  (** In the case that decryption fails, this exception is thrown. TODO encode more information*)
   
   val decrypt : ks:KS.t -> peer:Peer.t -> ciphertext:Cstruct.t -> iv:Cstruct.t -> Cstruct.t
-  (* Given a keying service, a peer, a cipher text and an initial vector return the decrypted 
-   * ciphertext *)
+  (** Given a keying service, a peer, a cipher text and an initial vector return the decrypted ciphertext *)
 end
--- src/Peer.mli
+++ src/Peer.mli
@@ -2,12 +2,12 @@ type t
 (* Internal representation for a peer *)
 
 val host : t -> string
-(* Gets host from a peer 
+(** Gets host from a peer 
  * TODO consider returning ipaddr or uri *)
 
 val port : t -> int
-(* Gets port from peer
+(** Gets port from peer
  * TODO consider if int is appropriate - probably is *)
 
 val compare : t -> t-> int
-(* Compare two peers - needed for Map.OrderedType satisfaction (keys in cache) *)
+(** Compare two peers - needed for Map.OrderedType satisfaction (keys in cache) *)
