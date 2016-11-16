87f7bbe3e8c020bd84137bcf6c1cdd8010366b49
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-16 17:25:24 +0000

    Documents peer better

--- src/Peer.mli
+++ src/Peer.mli
@@ -1,13 +1,15 @@
-(** Peers are endpoints in the system *)
+(** Provides a module to abstract the view endpoints/peers in the system *)
 
 type t
 (* Internal representation for a peer *)
 
 val host : t -> string
-(** Gets host from a peer. TODO consider returning ipaddr or uri *)
+(** [host peer] gives the string representing the host name of [peer] *)
 
 val port : t -> int
-(** Gets port from peer. TODO consider if int is appropriate - probably is *)
+(** [port peer] gives the integer representing the port number of [peer] *)
 
 val compare : t -> t-> int
-(** Compare two peers - needed for Map.OrderedType satisfaction (keys in cache) *)
+(** [compare p1 p2] is necessary for Peer.t to be used as a key in the [Lru_map.F] cache 
+implementation such that Peer.t satisfies the Map.OrderedType signature. This makes a uri from the
+peer and then uses the [Uri] module's compare function. *)
