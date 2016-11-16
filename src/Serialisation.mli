465913c4014bb305faae1b8006cc2524db0ef27a
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-16 20:26:20 +0000

    Adds base 64 encoding/decoding

--- src/Serialisation.mli
+++ src/Serialisation.mli
@@ -1,5 +1,11 @@
 (** Provides serialisation and deserialisation of Cstruct.t messages *)
 
+val encode : Cstruct.t -> string 
+(** [encode m] takes the [Cstruct.t] [m] and encodes it to a base 64 [string] for transmission *)
+
+val decode : string -> Cstruct.t
+(** [decode m] takes the base 64 [string] and attempts to decode it into a [Cstruct.t] *)
+
 (** Internal representation of messages passed around the system *)
 module Message : sig
 	type t
