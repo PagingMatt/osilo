9c2f097ff352bf50e37be27f57be83723b326e0b
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-16 20:59:06 +0000

    Finish off first implementation

---
+++ src/Serialisation.ml
@@ -0,0 +1,47 @@
+let encode m = 
+	m
+	|> Nocrypto.Base64.encode
+	|> Cstruct.to_string
+
+exception Decoding_failed
+
+let decode m =
+	match m |> Cstruct.of_string |> Nocrypto.Base64.decode with
+	| Some m' -> m'
+	| None    -> raise Decoding_failed
+
+module Message : sig
+	type t
+	exception Deserialisation_failed
+	val serialise   : t -> Cstruct.t
+	val deserialise : Cstruct.t -> t
+end = struct
+	type t =
+		| Data_rd
+		| Data_wr
+		| Capa_rq
+		| Capa_as
+
+	exception Deserialisation_failed
+
+	let serialise m =
+		(match m with
+		| Data_rd -> `Assoc [("type",`String "rd")]
+		| Data_wr -> `Assoc [("type",`String "wr")]
+		| Capa_rq -> `Assoc [("type",`String "rq")]
+		| Capa_as -> `Assoc [("type",`String "as")]) |> Yojson.Basic.to_string |> Cstruct.of_string
+
+    let deserialise m = 
+        let s = Cstruct.to_string m in
+		let j = Yojson.Basic.from_string s in
+		match Yojson.Basic.Util.member "type" j with
+		| `String x -> 
+			match x with
+			| "rd" -> Data_rd
+			| "wr" -> Data_wr
+			| "rq" -> Capa_rq
+			| "as" -> Capa_as
+			| _    -> raise Deserialisation_failed
+		| _ -> raise Deserialisation_failed
+end
+
--- src/Serialisation.mli
+++ src/Serialisation.mli
@@ -3,6 +3,9 @@
 val encode : Cstruct.t -> string 
 (** [encode m] takes the [Cstruct.t] [m] and encodes it to a base 64 [string] for transmission *)
 
+exception Decoding_failed
+(** Raised if cannot decode [Cstruct.t] *)
+
 val decode : string -> Cstruct.t
 (** [decode m] takes the base 64 [string] and attempts to decode it into a [Cstruct.t] *)
 
@@ -14,9 +17,9 @@ module Message : sig
 	exception Deserialisation_failed
 	(** Thrown when cannot parse deserialised meesage into a Message.t *)
 
-	val serialise : Message.t -> Cstruct.t 
+	val serialise : t -> Cstruct.t 
 	(** [serialise m] gives a [Cstruct.t] representing [m] *)
 
-	val deserialise : Cstruct.t -> Message.t
-	(** [deserialise m] gives a [Message.t] representing [m] *)
+	val deserialise : Cstruct.t -> t
+	(** [deserialise m] gives a [t] representing [m] *)
 end
