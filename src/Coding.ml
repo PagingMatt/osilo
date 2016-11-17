02675d032c73ffd7beb5bab75388cfb6ae37d6d9
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-17 23:13:27 +0000

    Renames cstruct specific

--- src/Coding.ml
+++ src/Coding.ml
@@ -1,11 +1,11 @@
 exception Decoding_failed
 
-let encode m = 
+let encode_cstruct m = 
   m
   |> Nocrypto.Base64.encode
   |> Cstruct.to_string
 
-let decode m =
+let decode_cstruct m =
   match m |> Cstruct.of_string |> Nocrypto.Base64.decode with
   | Some m' -> m'
   | None    -> raise Decoding_failed
@@ -15,14 +15,14 @@ let string_member s j =
   | `String m -> m
   | _         -> raise Decoding_failed
 
-let encode_encrypted ~ciphertext ~iv = 
+let encode_message ~ciphertext ~iv = 
   `Assoc [
-    ("ciphertext"     , `String (encode ciphertext));
-    ("initial_vector" , `String (encode iv        )) 
+    ("ciphertext"     , `String (encode_cstruct ciphertext));
+    ("initial_vector" , `String (encode_cstruct iv        )) 
   ] |> Yojson.Basic.to_string
 
-let decode_encrypted ~message =
+let decode_message ~message =
   let j = Yojson.Basic.from_string message in
-  let c = j |> string_member "ciphertext" |> decode in
-  let i = j |> string_member "initial_vector" |> decode in
+  let c = j |> string_member "ciphertext"     |> decode_cstruct in
+  let i = j |> string_member "initial_vector" |> decode_cstruct in
   (c,i)
