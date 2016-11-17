674f2659a2b83f9fcd41083b939150e5a3b9c3d5
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-17 23:10:13 +0000

    Implements functions

--- src/Coding.ml
+++ src/Coding.ml
@@ -1,11 +1,28 @@
-let encode m = 
-	m
-	|> Nocrypto.Base64.encode
-	|> Cstruct.to_string
-
 exception Decoding_failed
 
+let encode m = 
+  m
+  |> Nocrypto.Base64.encode
+  |> Cstruct.to_string
+
 let decode m =
-	match m |> Cstruct.of_string |> Nocrypto.Base64.decode with
-	| Some m' -> m'
-	| None    -> raise Decoding_failed
+  match m |> Cstruct.of_string |> Nocrypto.Base64.decode with
+  | Some m' -> m'
+  | None    -> raise Decoding_failed
+
+let string_member s j = 
+  match Yojson.Basic.Util.member s j with
+  | `String m -> m
+  | _         -> raise Decoding_failed
+
+let encode_encrypted ~ciphertext ~iv = 
+  `Assoc [
+    ("ciphertext"     , `String (encode ciphertext));
+    ("initial_vector" , `String (encode iv        )) 
+  ] |> Yojson.Basic.to_string
+
+let decode_encrypted ~message =
+  let j = Yojson.Basic.from_string message in
+  let c = j |> string_member "ciphertext" |> decode in
+  let i = j |> string_member "initial_vector" |> decode in
+  (c,i)
