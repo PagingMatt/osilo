5ced8eeed90219bab87ee0050e10667577e2c562
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-21 23:40:57 +0000

    Moves clients back into Client

--- src/Silo.ml
+++ src/Silo.ml
@@ -7,6 +7,21 @@ module Client : sig
   exception Failed_to_make_silo_client of Uri.t
   val make : server:Uri.t -> t
   val server : t -> string
+  module Silo_9p_client : sig
+    include Protocol_9p.Client.S
+    val connect:
+      string -> 
+      string -> 
+      ?msize:int32 -> 
+      ?username:string -> 
+      ?aname:string ->
+      unit -> 
+      t Protocol_9p.Error.t Lwt.t
+  end
+  module Silo_datakit_client : sig
+    include Datakit_S.CLIENT with type error = Protocol_9p_error.error
+    val connect : Silo_9p_client.t -> t
+  end
 end = struct
   type t = {
     server : string (* Silo_9p_client.connect takes a string not a Uri.t *)
@@ -29,23 +44,23 @@ end = struct
     { server = s }
 
   let server c = c.server
+
+  module Silo_9p_client = Client9p_unix.Make(
+    (val Logs.src_log (Logs.Src.create "osilo datakit client") : Logs.LOG) )
+  
+  module Silo_datakit_client = Datakit_client_9p.Make(Silo_9p_client)
 end
   
-module Silo_9p_client = Client9p_unix.Make(
-  (val Logs.src_log (Logs.Src.create "osilo datakit client") : Logs.LOG) )
-  
-module Silo_datakit_client = Datakit_client_9p.Make(Silo_9p_client)
-
 exception Checkout_failed
 exception Write_failed
 
 let checkout client service = 
-  Silo_9p_client.connect "tcp" (Client.server client) () 
+  Client.Silo_9p_client.connect "tcp" (Client.server client) () 
   >|= begin function
-      | Ok conn_9p  -> conn_9p |> Silo_datakit_client.connect
+      | Ok conn_9p  -> conn_9p |> Client.Silo_datakit_client.connect
       | Error error -> raise Checkout_failed
 	    end
-  >>= fun conn_dk -> Silo_datakit_client.branch conn_dk service 
+  >>= fun conn_dk -> Client.Silo_datakit_client.branch conn_dk service 
   >|= begin function
 	    | Ok branch   -> branch
 	    | Error error -> raise Checkout_failed
@@ -54,12 +69,12 @@ let checkout client service =
 let write ~client ~service ~file ~contents =
   checkout client service
   >>= fun branch -> 
-    Silo_datakit_client.Branch.with_transaction branch 
+    Client.Silo_datakit_client.Branch.with_transaction branch 
 	  (fun tr -> 
 	    let contents' = Yojson.Basic.to_string contents |> Cstruct.of_string in
-	    Silo_datakit_client.Transaction.create_file tr ~dir:(Datakit_path.of_string_exn file) "filename" contents' >>=
+	    Client.Silo_datakit_client.Transaction.create_file tr ~dir:(Datakit_path.of_string_exn file) "filename" contents' >>=
 	    begin function
-      | Ok ()   -> Silo_datakit_client.Transaction.commit tr "Write"
+      | Ok ()   -> Client.Silo_datakit_client.Transaction.commit tr "Write"
       | Error e -> raise Write_failed
 	    end)
   >|= begin function
@@ -70,8 +85,8 @@ let write ~client ~service ~file ~contents =
 let read ~client ~service ~file =
   checkout client service
   >>= fun branch -> 
-    Silo_datakit_client.Branch.with_transaction branch 
-	  (fun tr -> Silo_datakit_client.Transaction.read_file tr (Datakit_path.of_string_exn file))
+    Client.Silo_datakit_client.Branch.with_transaction branch 
+	  (fun tr -> Client.Silo_datakit_client.Transaction.read_file tr (Datakit_path.of_string_exn file))
   >|= begin function
       | Ok cstruct  -> Some (cstruct |> Cstruct.to_string |> Yojson.Basic.from_string)
       | Error error -> None
