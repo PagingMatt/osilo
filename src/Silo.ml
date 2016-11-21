5b76274b6a3b95ebb9409abbd2d40c156c963ade
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-21 21:09:18 +0000

    Write function

--- src/Silo.ml
+++ src/Silo.ml
@@ -1,7 +1,11 @@
+open Lwt.Infix
+open Result
+
 module Client : sig
   type t
   exception Failed_to_make_silo_client of Uri.t
   val make : server:Uri.t -> t
+  val server : t -> string
 end = struct
   type t = {
     server : string (* Silo_9p_client.connect takes a string not a Uri.t *)
@@ -23,8 +27,31 @@ end = struct
     let s = (Printf.sprintf "%s:%d" h p) in
     { server = s }
 
+  let server c = c.server
+
   module Silo_9p_client = Client9p_unix.Make(
   	val Logs.src (Logs.Src.create "osilo datakit client") : Logs.LOG)
   
   module Silo_datakit_client = Datakit_client_9p.Make(Silo_9p_client)
 end
+
+let write ~client ~service ~file ~contents =
+	Silo_9p_client.connect (Client.server client) () 
+	>>= begin function
+	  | Ok conn_9p  -> conn_9p |> Silo_datakit_client.connect
+	  | Error error -> raise Write_failed
+	  end
+	>>= fun conn_dk -> Silo_datakit_client.Branch service 
+	>>= begin function
+	  | Ok branch   -> branch
+	  | Error error -> raise Write_failed
+	  end
+	>>= fun branch -> 
+	  Silo_datakit_client.with_transaction branch 
+	  (fun tr -> 
+	    let contents' = Cstruct.of_string contents in
+	    Silo_datakit_client.Transaction.create_file tr (Datakit_path.of_string_exn file) contents' >>=
+	    begin function
+        | Ok ()   -> Silo_datakit_client.commit tr ~message:"Write"
+        | Error e -> raise Write_failed
+	    end)
