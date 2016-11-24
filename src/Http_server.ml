968626724e2d4769470d04b7a27a074fdef5f1cd
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-24 22:28:25 +0000

    Fixes indentation

--- src/Http_server.ml
+++ src/Http_server.ml
@@ -7,14 +7,16 @@ open Lwt.Infix
 class ping = object(self)
   inherit [Cohttp_lwt_body.t] Wm.resource
 
-  method content_types_provided rd = Wm.continue [("text/plain", self#to_text)] rd
+  method content_types_provided rd = 
+    Wm.continue [("text/plain", self#to_text)] rd
+
   method content_types_accepted rd = Wm.continue [] rd
   
   method allowed_methods rd = Wm.continue [`GET] rd
 
   method private to_text rd = 
-    let text = "pong"
-    in Wm.continue (`String (Printf.sprintf "%s" text)) rd
+    let text = "pong" in 
+    Wm.continue (`String (Printf.sprintf "%s" text)) rd
 end
 
 let api = [
@@ -24,11 +26,10 @@ let api = [
 let callback _ request body =
   Wm.dispatch' api ~body ~request 
   >|= begin function
-        | None -> (`Not_found, Cohttp.Header.init (), `String "Not found", [])
-        | Some result -> result 
+      | Some r -> r 
+      | None   -> (`Not_found, Cohttp.Header.init (), `String "Not found", [])
       end
-  >>= fun (status, headers, body, _) 
-      -> Server.respond ~headers ~status ~body ()
+  >>= fun (status, headers, body, _) -> Server.respond ~headers ~status ~body ()
 
 let start ~port =
   let server = Server.make ~callback () in
