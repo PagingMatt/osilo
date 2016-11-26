a85beb7a4a5820d7e8ba8aa8201cb4f22c18dc38
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-26 15:31:24 +0000

    Should be function

--- tests/peer_tests.ml
+++ tests/peer_tests.ml
@@ -2,13 +2,13 @@ let host = "127.0.0.1"
 let port = 8000
 let peer = Peer.create host port
 
-let peer_builds_with_host =
+let peer_builds_with_host () =
   Alcotest.(check string)
   "Checks host is stored and retrieved correctly from Peer"
   host
   (Peer.host peer)
 
-let peer_builds_with_port =
+let peer_builds_with_port () =
   Alcotest.(check int)
   "Checks port is stored and retrieved correctly from Peer"
   port
