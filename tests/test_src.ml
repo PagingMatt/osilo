cab3c92ef64040a779455a8f1a32a0f6a0dfa0c4
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-11-27 15:02:05 +0000

    Adds test for encrypted messages

--- tests/test_src.ml
+++ tests/test_src.ml
@@ -24,21 +24,38 @@ module Peer_tests = struct
 end
 
 module Coding_tests = struct
-  let b64 = "fooBARfooBARfooBARfooBARfooBARfo"
-  
+  let a = "fooBARfooBARfooBARfooBARfooBARfo"
+  let b = "FOObarFOObarFOObarFOObarFOObarFO" 
+
   let symm_cstruct () =
-    let c  = Coding.decode_cstruct b64 in
-    let s  = Coding.encode_cstruct c   in
-    let c' = Coding.decode_cstruct s   in
+    let c  = Coding.decode_cstruct a in
+    let s  = Coding.encode_cstruct c in
+    let c' = Coding.decode_cstruct s in
     Alcotest.(check string) 
       "Checks that starting from a base 64 string, decoding and re-encoding a cstruct is symmetric"
-      b64 s;
+      a s;
     Alcotest.(check cstruct)
       "Checks that starting from a Cstruct, encoding to a base 64 string and decoding back to a cstruct is symmetric"
       c c'
 
+  let symm_message () =
+    let c     = Coding.decode_cstruct a     in
+    let i     = Coding.decode_cstruct b     in
+    let s     = Coding.encode_message c i   in
+    let c',i' = Coding.decode_message s     in
+    let s'    = Coding.encode_message c' i' in
+    Alcotest.(check cstruct)
+      "Checks decoding and re-encoding a message produces the same ciphertext"
+      c c';
+    Alcotest.(check cstruct)
+      "Checks decoding and re-encoding a message produces the same initial vector"
+      i i';
+    Alcotest.(check string)
+      "Checks encoding and decoding a message produces the same string to send"
+      s s'
+
   let symm_dh_reply () =
-    let p  = Coding.decode_cstruct b64 in
+    let p  = Coding.decode_cstruct a   in
     let r  = Coding.encode_kx_reply p  in
     let p' = Coding.decode_kx_reply r  in
     let r' = Coding.encode_kx_reply p' in
@@ -51,10 +68,10 @@ module Coding_tests = struct
 
   let tests = [
     ("Tests that encoding/decoding cstructs is symmetric", `Quick, symm_cstruct);
-    (*("Tests that encoding/decoding DH groups is symmetric", `Quick, symm_group);
+    (*("Tests that encoding/decoding DH groups is symmetric", `Quick, symm_group);*)
     ("Tests that encoding/decoding encrypted messages is symmetric", `Quick, symm_message);
-    ("Tests that encoding/decoding DH key exchange init is symmetric", `Quick, symm_dh_init);*)
-    ("Tests that encoding/decoding DH key exchange reply is symmetric", `Quick, symm_dh_reply);
+    (*("Tests that encoding/decoding a DH key exchange init is symmetric", `Quick, symm_dh_init);*)
+    ("Tests that encoding/decoding a DH key exchange reply is symmetric", `Quick, symm_dh_reply)
   ]
 end
 
