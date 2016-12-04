f5f079b0308c9d0e9f169500991273d170686e58
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-12-04 19:18:29 +0000

    Removes from server implementation

--- server/Osilo_server.ml
+++ server/Osilo_server.ml
@@ -9,14 +9,12 @@ module Terminal = struct
         empty
         +> flag "-h" (required string)
         ~doc:"  Hostname of this server."
-        +> flag "-p" (optional_with_default 8000 int)
-        ~doc:"  Port to listen for REST API calls on."
         +> flag "-k" (required string)
         ~doc:"  Base 64 secret key shared with clients"
         +> flag "-ds" (required string)
         ~doc:"  Hostname of Datakit server"
       )
-      (fun h p k ds () -> Lwt_main.run (new Http_server.server h p (Coding.decode_cstruct k) ds )#start)
+      (fun h k ds () -> Lwt_main.run (new Http_server.server h (Coding.decode_cstruct k) ds )#start)
 
   let commands = 
     Command.group 
