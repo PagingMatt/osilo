b1967192f83bb532c2cc20ff8f54743e76b6d713
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-12-04 19:05:57 +0000

    Stops using port in Coding

--- src/Coding.ml
+++ src/Coding.ml
@@ -39,7 +39,6 @@ let tag_iv = "initial_vector"
 let tag_pb = "public"
 let tag_gr = "group"
 let tag_ho = "host"
-let tag_po = "port"
 let tag_me = "message"
 
 let encode_message' ~ciphertext ~iv = 
@@ -56,10 +55,8 @@ let decode_message' ~message =
 
 let encode_message ~peer ~ciphertext ~iv = 
   let host = Peer.host peer in
-  let port = Peer.port peer in
   `Assoc [
     (tag_ho , `String host);
-    (tag_po , `Int    port);
     (tag_ct , `String (encode_cstruct ciphertext));
     (tag_iv , `String (encode_cstruct iv        )) 
   ] |> Yojson.Basic.to_string
@@ -67,7 +64,6 @@ let encode_message ~peer ~ciphertext ~iv =
 let decode_message ~message =
   let j = Yojson.Basic.from_string message in
   let h = j |> string_member tag_ho in
-  let p = j |> int_member    tag_po in
   let c = j |> string_member tag_ct |> decode_cstruct in
   let i = j |> string_member tag_iv |> decode_cstruct in
   let peer = Peer.create h in
@@ -75,12 +71,10 @@ let decode_message ~message =
 
 let encode_kx_init ~peer ~public ~group =
   let host = Peer.host peer in
-  let port = Peer.port peer in
   let public' = encode_cstruct public in
   let group'  = encode_group   group  in
   `Assoc [
     (tag_ho, `String host);
-    (tag_po, `Int    port);
     (tag_pb, `String public'); 
     (tag_gr, `String group')
   ] |> Yojson.Basic.to_string
@@ -88,7 +82,6 @@ let encode_kx_init ~peer ~public ~group =
 let decode_kx_init ~message =
   let j  = Yojson.Basic.from_string message in
   let h  = j |> string_member tag_ho in
-  let p  = j |> int_member    tag_po in
   let k  = j |> string_member tag_pb in
   let k' = decode_cstruct k          in
   let g  = j |> string_member tag_gr in
@@ -98,11 +91,9 @@ let decode_kx_init ~message =
 
 let encode_kx_reply ~peer ~public =
   let host = Peer.host peer in
-  let port = Peer.port peer in
   let public' = encode_cstruct public in
   `Assoc [
     (tag_ho, `String host);
-    (tag_po, `Int    port);
     (tag_pb, `String public')
   ]
   |> Yojson.Basic.to_string
@@ -110,7 +101,6 @@ let encode_kx_reply ~peer ~public =
 let decode_kx_reply ~message =
   let j = Yojson.Basic.from_string message in
   let h = j |> string_member tag_ho in
-  let p = j |> int_member    tag_po in
   let k = j |> string_member tag_pb in
   let k' = decode_cstruct k in 
   let peer = Peer.create h  in
