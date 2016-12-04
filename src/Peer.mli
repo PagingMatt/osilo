90c8c485a406a3e7c3542284312228ef6f4d2ebc
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-12-04 19:26:16 +0000

    Corrects peer docs

--- src/Peer.mli
+++ src/Peer.mli
@@ -4,12 +4,12 @@ type t
 (* Internal representation for a peer *)
 
 val create : string -> t
-(** [create host] builds a [t] from a string host *)
+(** [create host] builds a [t] from a string [host] *)
 
 val host : t -> string
 (** [host peer] gives the string representing the host name of [peer] *)
 
 val compare : t -> t-> int
-(** [compare p1 p2] is necessary for Peer.t to be used as a key in the [Lru_map.F] cache 
+(** [compare p1 p2] is necessary for Peer.t to be used as a key in the [Lru.F] cache 
 implementation such that Peer.t satisfies the Map.OrderedType signature. This makes a uri from the
 peer and then uses the [Uri] module's compare function. *)
