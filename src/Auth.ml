6c84bec364357c95e524f476248789b698e4dbe2
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-12-08 23:50:58 +0000

    Adds capability module

--- src/Auth.ml
+++ src/Auth.ml
@@ -1,3 +1,27 @@
+module Capability = struct
+  type token =  R | W
+
+  type t = {
+    service : string ;
+    path    : string ;
+    token   : token  ;
+  } with sexp
+
+  let create_read s p =
+    {service = s; path = p; token = R;}
+
+  let create_write s p =
+    {service = s; path = p; token = W;}
+
+  let to_string c = 
+    sexp_of_t c
+    |> Sexp.to_string
+
+  let of_string s =
+    Sexp.of_string s 
+   |> t_of_sexp
+end
+
 module Crypto : Macaroons.CRYPTO = struct
   let hmac ~key message =
     Nocrypto.Hash.SHA512.hmac
