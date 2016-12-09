6c6932bb58ded494966f648d348c6c3c80626c84
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-12-09 15:11:46 +0000

    Macaroons instantiation before Capability

--- src/Auth.ml
+++ src/Auth.ml
@@ -1,27 +1,3 @@
-module Capability = struct
-  type token =  R | W
-
-  type t = {
-    service : string ;
-    path    : string ;
-    token   : token  ;
-  } with sexp
-
-  let create_read s p =
-    {service = s; path = p; token = R;}
-
-  let create_write s p =
-    {service = s; path = p; token = W;}
-
-  let to_string c = 
-    sexp_of_t c
-    |> Sexp.to_string
-
-  let of_string s =
-    Sexp.of_string s 
-   |> t_of_sexp
-end
-
 module Crypto : Macaroons.CRYPTO = struct
   let hmac ~key message =
     Nocrypto.Hash.SHA512.hmac
@@ -51,3 +27,28 @@ module Crypto : Macaroons.CRYPTO = struct
 end
 
 module M = Macaroons.Make(Crypto)
+
+module Capability = struct
+  type token =  R | W
+
+  type t = {
+    service : string ;
+    path    : string ;
+    token   : token  ;
+  } with sexp
+
+  let create_read s p =
+    {service = s; path = p; token = R;}
+
+  let create_write s p =
+    {service = s; path = p; token = W;}
+
+  let to_string c = 
+    sexp_of_t c
+    |> Sexp.to_string
+
+  let of_string s =
+    Sexp.of_string s 
+   |> t_of_sexp
+end
+
