49e3aa9445a7250fd208eb44df19b702a4b17560
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-12-10 15:57:32 +0000

    Spacing in Peer.compare

--- src/Peer.mli
+++ src/Peer.mli
@@ -9,7 +9,7 @@ val create : string -> t
 val host : t -> string
 (** [host peer] gives the string representing the host name of [peer] *)
 
-val compare : t -> t-> int
+val compare : t -> t -> int
 (** [compare p1 p2] is necessary for Peer.t to be used as a key in the [Lru.F] cache 
 implementation such that Peer.t satisfies the Map.OrderedType signature. This makes a uri from the
 peer and then uses the [Uri] module's compare function. *)
