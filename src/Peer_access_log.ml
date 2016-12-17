d799966ffcfdbd0e05862dd4a9b297da1edc109d
Matt Harrison <matt.anthony.harrison@gmail.com>
2016-12-17 17:28:48 +0000

    Implement access log

---
+++ src/Peer_access_log.ml
@@ -0,0 +1,19 @@
+open Core.Std 
+
+type t = (Peer.t list) File_tree.t 
+
+let empty = File_tree.empty
+
+let build_loc = 
+  fun h -> fun s -> fun p -> 
+  (fun _ -> String.split (Printf.sprintf "%s/%s/%s" (Peer.host h) s p) ~on:'/')
+
+let build_el =
+  fun ps1 -> fun ps2 -> List.append ps1 ps2 |> List.dedup ~compare:Peer.compare
+
+let log l ~host ~peer ~service ~path =
+  File_tree.insert ~element:[peer] ~tree:l ~location:(build_loc host service path) ~select:build_el
+
+let find l ~host ~service ~path = 
+  File_tree.flatten_under ~tree:l ~path:(String.split (Printf.sprintf "%s/%s/%s" (Peer.host host) service path) ~on:'/')
+  |> List.fold ~init:[] ~f:List.append 
--- src/Peer_access_log.mli
+++ src/Peer_access_log.mli
@@ -1,11 +1,11 @@
 type t 
 
-val create : t
+val empty : t
 
-val log : t -> peer:Peer.t -> service:string -> path:string -> t
-(** [log l ~peer ~service ~path] takes the current peer access log and gives back another where it 
-has been recorded that [peer] requested [path] from my [service]. *)
+val log : t -> host:Peer.t -> peer:Peer.t -> service:string -> path:string -> t
+(** [log l ~host ~peer ~service ~path] takes the current peer access log and gives back another where it 
+has been recorded that [peer] requested [path] from [host]'s' [service]. *)
 
-val find : t -> service:string -> path:string -> Peer.t list
-(** [find l ~service ~path] returns a list of [Peer.t] which have accessed [path] or any file below
-[path] in my [service]. *)
+val find : t -> host:Peer.t -> service:string -> path:string -> Peer.t list
+(** [find l ~host ~service ~path] returns a list of [Peer.t] which have accessed [path] or any file below
+[path] in [host]'s [service]. *)
