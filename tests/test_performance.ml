1423f0e2bd28c1ab4e58b8d0563cf4951147088f
Matt Harrison <matt.anthony.harrison@gmail.com>
2017-01-07 19:47:28 +0000

    Shows the slow down in verification is due to macaroons
    Verification is linear with about 26us per macaroon

--- tests/test_performance.ml
+++ tests/test_performance.ml
@@ -16,14 +16,34 @@ let worst_case_args =
 let capabilities =
   Auth.mint peer key service tokpaths
 
+let paths = ["erwfg";"werg";"wrt";"yjtr";"asd";"irut";"oiyt";"f3"]
+let _,caps = List.unzip capabilities
+
 let tree = 
   List.fold ~init:Auth.CS.empty capabilities
     ~f:(fun s -> fun (t,c) -> Auth.CS.record_if_most_general s (t |> token_of_string) c)
 
 let () = Command.run (Bench.make_command [
   Bench.Test.create_indexed
+    ~name:"Building CS tree"
+    ~args:[1;2;3;4;5;6;7;8]
+    (fun num -> Staged.stage 
+      (fun () -> ignore (List.fold ~init:Auth.CS.empty (List.take capabilities num)
+        ~f:(fun s -> fun (t,c) -> Auth.CS.record_if_most_general s (t |> token_of_string) c))));
+  Bench.Test.create_indexed
+    ~name:"Verifying capabilities"
+    ~args:[1;2;3;4;5;6;7;8]
+    (fun num -> Staged.stage 
+      (fun () -> 
+        ignore (List.map (List.take caps num) ~f:(Auth.verify R (key |> Coding.encode_cstruct)))));
+  Bench.Test.create_indexed
     ~name:"Worst case capability selection"
     ~args:[1;2;3;4;5;6;7;8]
     (fun num -> Staged.stage 
-      (fun () -> ignore (Auth.find_permissions tree (List.take worst_case_args num))))
+      (fun () -> ignore (Auth.find_permissions tree (List.take worst_case_args num))));
+  Bench.Test.create_indexed
+    ~name:"Worst case capability verification"
+    ~args:[1;2;3;4;5;6;7;8]
+    (fun num -> Staged.stage 
+      (fun () -> ignore (Auth.authorise (List.take paths num) (List.take caps num) R key peer service)))
 ])
