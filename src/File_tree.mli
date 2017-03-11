0d4b6b1f4f3ab584ca11f44326d5984e7c29549a
Matt Harrison <matt.anthony.harrison@gmail.com>
2017-03-11 19:01:37 +0000

    Adds flattening functions

--- src/File_tree.mli
+++ src/File_tree.mli
@@ -7,18 +7,18 @@ exception Path_empty
 (** Raised when inserting and hit an empty path, this should never happen as the terminating
 condition is on a singleton list, but if fed an empty path need to catch this. *)
 
-val empty : 'a t 
+val empty : 'a t
 
-val insert : 
-  element:   'a                       -> 
-  tree:      'a t                     -> 
-  location: ('a -> string list)       -> 
-  select:   ('a -> 'a -> 'a)          -> 
+val insert :
+  element:   'a                       ->
+  tree:      'a t                     ->
+  location: ('a -> string list)       ->
+  select:   ('a -> 'a -> 'a)          ->
   terminate:('a option -> 'a -> bool) -> 'a t
 (** [insert ~element ~tree ~location ~select ~terminate] give the tree with [element] inserted into [tree],
 provided for each step down the path, [terminate elopt el] returns false.
 The place in the file tree that [element] is inserted into is determined by [location] which maps
-the element to a list of directories which nest down to the target location. When at the target 
+the element to a list of directories which nest down to the target location. When at the target
 location, if this already exists, [select] compares [element] and the element currently held there
 it then gives back the element which should be at the position. *)
 
@@ -26,16 +26,20 @@ val shortest_path_match :
   tree:       'a t            ->
   location:       string list ->
   satisfies: ('a -> bool)     -> 'a option
-(** [shortest_path_match ~tree ~location ~satisfies] starts at the root of [tree] and walks down 
+(** [shortest_path_match ~tree ~location ~satisfies] starts at the root of [tree] and walks down
 towards [location] until an element along [location] in [tree] satisfies the predicate [satisfies], it then
 returns this element. If it reaches a leaf before finding a satisfying element [None] is returned. *)
 
 exception Trim_failed
 (** Raised if [get_min] behaves unexpectadly during deletion. *)
 
+val flatten : tree:'a t -> 'a list
+
+val flatten_below : tree:'a t -> location:string list -> 'a list
+
 val trim :
   tree:    'a t         ->
   location: string list -> 'a list * 'a t
-(** Walks down to the node at [location] in [tree] and returns the new tree with this node and it's 
+(** Walks down to the node at [location] in [tree] and returns the new tree with this node and it's
 sub tree removed, but any left and right nodes still remaining. The contents of the subtree is flattened
 and passed back in a pair. *)
