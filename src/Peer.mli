610e533001149ecc46724fc15aadd862de76bf48
Matt Harrison <matt.anthony.harrison@gmail.com>
2017-04-08 16:12:09 +0100

    Documents peer

--- src/Peer.mli
+++ src/Peer.mli
@@ -1,15 +1,14 @@
-(** Provides a module to abstract the view endpoints/peers in the system *)
+(** Provides a module to abstract peers in the platform. *)
 
 type t
-(* Internal representation for a peer *)
+(* Internal representation for a peer. *)
 
 val create : string -> t
-(** [create host] builds a [t] from a string [host] *)
+(** [create host] builds a [t] from a string [host]. *)
 
 val host : t -> string
-(** [host peer] gives the string representing the host name of [peer] *)
+(** [host peer] gives the [string] representing the host name of [peer]. *)
 
 val compare : t -> t -> int
-(** [compare p1 p2] is necessary for Peer.t to be used as a key in the [Lru.F] cache 
-implementation such that Peer.t satisfies the Map.OrderedType signature. This makes a uri from the
-peer and then uses the [Uri] module's compare function. *)
+(** This makes a uri from the peer and then uses the [Uri] module's compare
+    function. *)
