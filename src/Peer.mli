1b598b10407073974f4cc1a0b0ab675445ca96e0
Matt Harrison <matt.anthony.harrison@gmail.com>
2017-05-02 13:17:30 +0100

    Adds port to Peer

--- src/Peer.mli
+++ src/Peer.mli
@@ -3,11 +3,14 @@
 type t
 (* Internal representation for a peer. *)
 
-val create : string -> t
-(** [create host] builds a [t] from a string [host]. *)
+val create : string -> int -> t
+(** [create host port] builds a [t] from a string [host] and int [port]. *)
 
 val host : t -> string
 (** [host peer] gives the [string] representing the host name of [peer]. *)
+
+val port : t -> int
+(** [port peer] gives the [int] representing the port number of [peer]. *)
 
 val compare : t -> t -> int
 (** This makes a uri from the peer and then uses the [Uri] module's compare
